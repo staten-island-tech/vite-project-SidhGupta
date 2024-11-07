6ca6ca903eb3d0f66fa02ff6007c1c044b0888aa
SidhGupta <158204477+SidhGupta@users.noreply.github.com>
2024-11-07 11:29:30 -0500

    Update main.js

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -1,5 +1,16 @@
 import '../css/style.css'
-import { pokemonInfo } from "./products"
+import { pokemonInfo } from "./products.js"
 import { DOMSelectors } from './dom.js'
+
+function addCard() {
+    pokemonInfo.forEach((pokemon) => DOMSelectors.container.insertAdjacentHTML('beforeend',
+        `<div class="card">
+            <h3 class="card-header">${pokemon.title}</h3>
+            <img src="${pokemon.pokedexNumber}" alt="">
+            <p>${pokemon.type}</p>
+        </div>`
+    ))
+    
+}
 
 
